5391dd872b1f3f218ccd42ac20ca43f7cdea535e
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-08-09 08:26:59 -0600

    Change core data model

--- Arrowhead/ContentView.swift
+++ Arrowhead/ContentView.swift
@@ -12,32 +12,31 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \Bookmark.data, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var bookmarks: FetchedResults<Bookmark>
 
     var body: some View {
         List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+            ForEach(bookmarks) { bookmark in
+                // TODO: show the folder name here
+                Text("Some bookmark")
             }
             .onDelete(perform: deleteItems)
         }
         .toolbar {
-            #if os(iOS)
             EditButton()
-            #endif
 
             Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
+                Label("Add Location", systemImage: "plus")
             }
         }
     }
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+            let newBookmark = Bookmark(context: viewContext)
+            newBookmark.data = Data()
 
             do {
                 try viewContext.save()
@@ -52,7 +51,7 @@ struct ContentView: View {
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
+            offsets.map { bookmarks[$0] }.forEach(viewContext.delete)
 
             do {
                 try viewContext.save()
